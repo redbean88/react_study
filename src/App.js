01b49c5e984e6315b8ef5c3d3eb4f96d05521f1b
redbean <dackpro00@gmail.com>
2021-10-11 05:12:03 +0900

    배열에 항목 제거하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,65 @@
-import React from "react";
+import React, {useRef , useState } from "react";
 import UesrList from "./UserList";
-import './App.css'
+import CreateUser from "./CreateUser";
 
 function App() {
+  const [inputs , setInputs] = useState({
+    username: '',
+    email: '',
+  })
+  const {username , email} = inputs;
+  const onChange = e => {
+    const {name , value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    })
+  }
+
+  const [users, setUsers] = useState([
+    {
+        id: 1,
+        username: 'tester',
+        email: 'test@test.com',
+    },
+    {
+        id: 2,
+        username: 'tester1',
+        email: 'test1@test.com',
+    },
+    {
+        id: 3,
+        username: 'tester2',
+        email: 'test2@test.com',
+    }
+]);
+
+const nextId = useRef(4)
+
+const onCreate = () => {
+  const user = {
+    id: nextId.current,
+    username,
+    email,
+  }
+  // setUsers([...users, user]);
+  setUsers(users.concat(user));
+  setInputs({
+    username:'',
+    email:''
+  });
+  nextId.current += 1;
+}
+
+const onRemove = (id) => {
+  setUsers(users.filter(user => user.id !== id));
+};
+
   return (
-    <UesrList/>
+    <>
+      <CreateUser username={username} email = {email} onChange = {onChange} onCreate = {onCreate}/>
+      <UesrList users={users} onRemove={onRemove}/>
+    </>
   );
 }
 
--- src/UserList.js
+++ src/UserList.js
@@ -1,33 +1,22 @@
 import React from "react";
 
-function User({user}){
-    return (<div>{user.username}<span>({user.email})</span>
-    </div>)
+function User({user , onRemove}){
+    const {username , email , id} = user;
+    return (
+    <div>{username}<span>({email})</span>
+    <button onClick={() => onRemove(id)}>삭제</button>
+    </div>
+    
+    )
 }
 
-function UesrList(){
-    const users = [
-        {
-            id: 1,
-            username: 'tester',
-            email: 'test@test.com',
-        },
-        {
-            id: 2,
-            username: 'tester1',
-            email: 'test1@test.com',
-        },
-        {
-            id: 3,
-            username: 'tester2',
-            email: 'test2@test.com',
-        }
-    ]
+function UesrList({users , onRemove}){
+    
     return (
     <div>
         {
             users.map(
-                (user, index) => (<User user={user} key={user.id}/>)
+                (user, index) => (<User user={user} key={user.id} onRemove={onRemove}/>)
             )
         }
     </div>
