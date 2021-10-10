afe09285ada32bddd91e7370503a341393657e59
redbean <dackpro00@gmail.com>
2021-10-11 05:20:16 +0900

    16 배열에 항목 수정하기

--- src/App.js
+++ src/App.js
@@ -21,16 +21,19 @@ function App() {
         id: 1,
         username: 'tester',
         email: 'test@test.com',
-    },
-    {
+        active: true,
+      },
+      {
         id: 2,
         username: 'tester1',
         email: 'test1@test.com',
-    },
-    {
+        active: false,
+      },
+      {
         id: 3,
         username: 'tester2',
         email: 'test2@test.com',
+        active: false,
     }
 ]);
 
@@ -55,10 +58,14 @@ const onRemove = (id) => {
   setUsers(users.filter(user => user.id !== id));
 };
 
+const onToggle = id => {
+  setUsers(users.map(user => user.id === id ? {...user, active: !user.active} : user))
+}
+
   return (
     <>
       <CreateUser username={username} email = {email} onChange = {onChange} onCreate = {onCreate}/>
-      <UesrList users={users} onRemove={onRemove}/>
+      <UesrList users={users} onRemove={onRemove} onToggle={onToggle}/>
     </>
   );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -1,22 +1,22 @@
 import React from "react";
 
-function User({user , onRemove}){
-    const {username , email , id} = user;
+function User({user , onRemove, onToggle}){
+    const {username , email , id, active} = user;
     return (
-    <div>{username}<span>({email})</span>
+    <div onClick={() => onToggle(id)} style={{color: active ? 'green' : 'black', cursor:'pointer'}}>{username}<span>({email})</span>
     <button onClick={() => onRemove(id)}>삭제</button>
     </div>
     
     )
 }
 
-function UesrList({users , onRemove}){
+function UesrList({users , onRemove, onToggle}){
     
     return (
     <div>
         {
             users.map(
-                (user, index) => (<User user={user} key={user.id} onRemove={onRemove}/>)
+                (user, index) => (<User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle}/>)
             )
         }
     </div>
