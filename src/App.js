1fc87a53a63b4e1d5690eba3a674178b83798b55
redbean <dackpro00@gmail.com>
2021-10-11 12:25:32 +0900

    18 useMemo 를 사용하여 연산한 값 재사용하기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React, {useRef , useState } from "react";
+import React, {useRef , useState, useMemo, useCallback} from "react";
 import UesrList from "./UserList";
 import CreateUser from "./CreateUser";
+
+function countActiveUsers(users){
+  console.log('사용자 카운팅 중')
+  return users.filter(user => user.active).length;
+}
 
 function App() {
   const [inputs , setInputs] = useState({
@@ -8,13 +13,14 @@ function App() {
     email: '',
   })
   const {username , email} = inputs;
-  const onChange = e => {
+  
+  const onChange = useCallback(e => {
     const {name , value } = e.target;
     setInputs({
       ...inputs,
       [name]: value
     })
-  }
+  }, [inputs]);
 
   const [users, setUsers] = useState([
     {
@@ -39,7 +45,7 @@ function App() {
 
 const nextId = useRef(4)
 
-const onCreate = () => {
+const onCreate = useCallback(() => {
   const user = {
     id: nextId.current,
     username,
@@ -52,20 +58,23 @@ const onCreate = () => {
     email:''
   });
   nextId.current += 1;
-}
+}, [username, email, users]);
 
-const onRemove = (id) => {
+const onRemove = useCallback((id) => {
   setUsers(users.filter(user => user.id !== id));
-};
+},[users]);
 
-const onToggle = id => {
+const onToggle = useCallback(id => {
   setUsers(users.map(user => user.id === id ? {...user, active: !user.active} : user))
-}
+},[users])
+
+const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
     <>
       <CreateUser username={username} email = {email} onChange = {onChange} onCreate = {onCreate}/>
       <UesrList users={users} onRemove={onRemove} onToggle={onToggle}/>
+      <div>활성 사용자수 : {count}</div>
     </>
   );
 }
