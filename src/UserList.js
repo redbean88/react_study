ef0f2a6c24922c75a3133c2129a4422a119f6c77
redbean <dackpro00@gmail.com>
2021-10-12 22:30:47 +0900

    25,26 리듀서 immer 로 구현하기

--- src/UserList.js
+++ src/UserList.js
@@ -1,43 +1,43 @@
-import React , {useContext} from "react";
-import { UserDispatch } from "./App"
+import React, { useContext } from 'react';
+import { UserDispatch } from './App';
 
-const User = React.memo(function User({user }){
-    const {username , email , id, active} = user;
-    const dispatch = useContext(UserDispatch);
+const User = React.memo(function User({ user }) {
+  const dispatch = useContext(UserDispatch);
 
-    return (
-    <div 
-        style={{
-            color: active ? 'green' : 'black',
-            cursor:'pointer'
-        }}
-        onClick={() => dispatch({
-            type: 'TOGGLE_USER',
-            id
-        })}
-    >{username}<span>({email})</span>
-    <button 
-        onClick={() => dispatch({
-            type: 'REMOVE_USER' ,
-            id
-        })}
-    >삭제</button>
-    </div>
-    
-    )
-})
-
-function UesrList({users }){
-    
-    return (
+  return (
     <div>
-        {
-            users.map(
-                (user, index) => (<User user={user} key={user.id}/>)
-            )
-        }
+      <b
+        style={{
+          cursor: 'pointer',
+          color: user.active ? 'green' : 'black'
+        }}
+        onClick={() => {
+          dispatch({ type: 'TOGGLE_USER', id: user.id });
+        }}
+      >
+        {user.username}
+      </b>
+      &nbsp;
+      <span>({user.email})</span>
+      <button
+        onClick={() => {
+          dispatch({ type: 'REMOVE_USER', id: user.id });
+        }}
+      >
+        삭제
+      </button>
     </div>
-    );
+  );
+});
+
+function UserList({ users }) {
+  return (
+    <div>
+      {users.map(user => (
+        <User user={user} key={user.id} />
+      ))}
+    </div>
+  );
 }
 
-export default React.memo(UesrList)
+export default React.memo(UserList);
